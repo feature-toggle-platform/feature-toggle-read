fb91c6e36c7b9baaa206ba745a7a1dd4b603a8f6
SaQie <Kamcio99@interia.com>
2026-02-16 17:33:17 +0100

    fix after dev tests

--- src/main/java/pl/feature/toggle/service/read/infrastructure/security/Config.java
+++ src/main/java/pl/feature/toggle/service/read/infrastructure/security/Config.java
@@ -3,6 +3,9 @@ package pl.feature.toggle.service.read.infrastructure.security;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.context.annotation.RequestScope;
 import pl.feature.toggle.service.model.security.actor.ActorProvider;
 import pl.feature.toggle.service.model.security.correlation.CorrelationProvider;
@@ -21,4 +24,15 @@ class Config {
         return new HttpCorrelationProvider(request);
     }
 
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
+                        .anyRequest().permitAll()
+                )
+//                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+                .build();
+    }
 }
